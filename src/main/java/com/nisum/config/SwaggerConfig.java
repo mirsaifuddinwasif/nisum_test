c993498e089cf6be26a5d24cc4b3039d1edffd80
pdamaso-nisum-com <pdamaso@nisum.com>
2019-11-22 11:21:34 -0300

    Update SwaggerConfig to fix request selector

--- src/main/java/com/nisum/config/SwaggerConfig.java
+++ src/main/java/com/nisum/config/SwaggerConfig.java
@@ -20,7 +20,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.nisum.test"))
+                .apis(RequestHandlerSelectors.basePackage("com.nisum"))
                 .paths(Predicates.not(PathSelectors.regex("/error")))
                 .build()
                 .apiInfo(getApiInfo());
